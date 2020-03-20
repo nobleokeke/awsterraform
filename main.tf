6f79c1353df77c6164a7b6a3ee6c0352f7de6fbe
Richard Koranteng <richard@rkkoranteng.com>
2020-03-20 00:27:32 -0400

    variables in main.tf

--- main.tf
+++ main.tf
@@ -2,38 +2,38 @@ resource "aws_instance" "server" {
   ami = "ami-0c322300a1dd5dc79"
 
   # INSTACNE TYPE
-  instance_type = "t2.micro"
+  instance_type = "${var.ec2-type}"
 
   # CONFIG INSTANCE
-  availability_zone = "us-east-1e"
+  availability_zone = "${var.availability-zone}"
 
   # ADD STORAGE
   root_block_device {
     delete_on_termination = "true"
-    volume_size = "40"
-    volume_type = "standard"
+    volume_size = "${var.root-ebs-size}"
+    volume_type = "${var.root-ebs-type}"
   }
 
   ebs_block_device {
     delete_on_termination = "true"
-    device_name = "/dev/sdb"
-    volume_size = "80"
-    volume_type = "standard"
+    device_name = "${var.data-ebs-device}"
+    volume_size = "${var.data-ebs-size}"
+    volume_type = "${var.data-ebs-type}"
   }
 
   # ADD TAGS
   tags = {
-    Name = "NobleDB"
+    Name = "${var.host-tag}"
   }
 
   volume_tags = {
-    Name =  "NobleDB"
+    Name =  "${var.host-tag}"
   }
 
   # SECURITY GROUPS
-  security_groups = ["DemoSecurityGroup"]
+  security_groups = ["${var.security-group}"]
 
   # SSH KEY
-  key_name = "DevopsKey"
+  key_name = "${var.ec2-key}"
 
 }
